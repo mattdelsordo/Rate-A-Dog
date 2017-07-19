a3cd19d4fe072bf24843a1c742503599099e0345
Matt DelSordo <mattdelsordo@gmail.com>
2017-07-19 12:20:00 -0400

    Talking to AWS now works! Yay!

--- app/src/main/java/com/mdelsordo/rate_a_dog/model/AWSUtil.java
+++ app/src/main/java/com/mdelsordo/rate_a_dog/model/AWSUtil.java
@@ -5,18 +5,15 @@ import android.util.Log;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.CognitoCachingCredentialsProvider;
-import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.rekognition.AmazonRekognition;
-import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
-import com.amazonaws.services.rekognition.model.AmazonRekognitionException;
+import com.amazonaws.services.rekognition.AmazonRekognitionClient;
 import com.amazonaws.services.rekognition.model.DetectLabelsRequest;
 import com.amazonaws.services.rekognition.model.DetectLabelsResult;
 import com.amazonaws.services.rekognition.model.Image;
 import com.amazonaws.services.rekognition.model.Label;
+import com.amazonaws.services.s3.model.Region;
 import com.mdelsordo.rate_a_dog.util.Logger;
 
 import java.nio.ByteBuffer;
@@ -35,37 +32,28 @@ public class AWSUtil {
     private static final float MIN_CONFIDENCE = 80F;
     private static final int MAX_LABELS = 10;
 
-    //accepts image and retuns a ByteBuffer
-//    public static ByteBuffer imageToBytes(){
-//
-//    }
+    private static final String COGNITO_POOL_ID = "us-east-1:838d53dc-d050-4e10-8856-0f1c6642c431";
+    private static final Regions COGNITO_REGION = Regions.US_EAST_1;
 
     //accepts bytes and returns a collection of labels for the converted image
-    public static Collection<Label> detectLabelsTest(byte[] bytes, Context context) throws AmazonRekognitionException{
+    public static Collection<Label> detectLabelsTest(byte[] bytes, Context context){
         AWSCredentials credentials;
-//        try {
-//            //!!!!TODO: This will not work I need to figure out how to send the ACTUAL credentials!!!!
-//            credentials = new EnvironmentVariableCredentialsProvider().getCredentials();
-//        } catch(Exception e) {
-//            throw new AmazonClientException("Cannot load the credentials from the credential profiles file. "
-//                    + "Please make sure that your credentials file is at the correct "
-//                    + "location (/Users/userid/.aws/credentials), and is in a valid format.", e);
-//        }
 
         CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
                 context.getApplicationContext(),
-                "us-east-1:838d53dc-d050-4e10-8856-0f1c6642c431", // Identity pool ID
-                Regions.US_EAST_1 // Region
+                COGNITO_POOL_ID, // Identity pool ID
+                COGNITO_REGION // Region
         );
         credentials = credentialsProvider.getCredentials();
 
         ByteBuffer buffer = ByteBuffer.wrap(bytes);
 
-        AmazonRekognition rekognitionClient = AmazonRekognitionClientBuilder
-                .standard()
-                .withRegion(Regions.US_WEST_2)
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                .build();
+//        AmazonRekognition rekognitionClient = AmazonRekognitionClientBuilder
+//                .standard()
+//                .withRegion(Regions.US_WEST_2)
+//                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+//                .build();
+        AmazonRekognition rekognitionClient = new AmazonRekognitionClient(credentials);
 
         DetectLabelsRequest request = new DetectLabelsRequest()
                 .withImage(new Image().withBytes(buffer))
@@ -84,10 +72,9 @@ public class AWSUtil {
     }
 
     //searches collection of labels for instances of "dog"
-    //TODO: i am 0% confident thats how that works at all
     public static boolean containsDog(Collection<Label> labels){
         for(Label l : labels){
-            if(l.getName().equals("dog")) return true;
+            if(l.getName().equals("Dog")) return true;
         }
         return false;
     }
