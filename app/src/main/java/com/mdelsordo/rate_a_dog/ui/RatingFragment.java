f128a9f19b54186f67eac3081086603454be436e
Matt DelSordo <mattdelsordo@gmail.com>
2017-07-20 10:29:37 -0400

    Enabled sharing image of rating on social media.

--- app/src/main/java/com/mdelsordo/rate_a_dog/ui/RatingFragment.java
+++ app/src/main/java/com/mdelsordo/rate_a_dog/ui/RatingFragment.java
@@ -2,11 +2,18 @@ package com.mdelsordo.rate_a_dog.ui;
 
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,7 +24,6 @@ import android.widget.Toast;
 
 import com.mdelsordo.rate_a_dog.R;
 import com.mdelsordo.rate_a_dog.util.Logger;
-import com.transitionseverywhere.TransitionManager;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
@@ -47,7 +53,8 @@ public class RatingFragment extends Fragment {
     private Button mBack;
     private ImageView mPhoto;
     private TextView mRating;
-    private ViewGroup mRatingGroup;
+    private FloatingActionButton mShare;
+    private View mRatingCard;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -59,16 +66,18 @@ public class RatingFragment extends Fragment {
         boolean isGood = args.getBoolean(ARG_GOOD);
         String path = args.getString(ARG_PHOTO);
 
-        mRatingGroup = (ViewGroup)view.findViewById(R.id.vg_rating_text);
+        //get layout of card
+        mRatingCard = view.findViewById(R.id.cv_rating_maincard);
 
         //set photo image
         mPhoto = (ImageView)view.findViewById(R.id.iv_rating_photo);
         try{
             InputStream stream = getContext().getContentResolver().openInputStream(Uri.parse(path));
-            mPhoto.setImageBitmap(BitmapFactory.decodeStream(stream));
+            Bitmap dog = BitmapFactory.decodeStream(stream);
+            mPhoto.setImageBitmap(dog);
         }catch (FileNotFoundException e){
             Logger.e(TAG, e.toString());
-            mPhoto.setImageBitmap(null);
+            mPhoto.setImageResource(R.drawable.default_dog);
             Toast.makeText(getContext(), "ERROR: File not found.", Toast.LENGTH_LONG).show();
             mListener.ratingBack();
         }
@@ -87,14 +96,48 @@ public class RatingFragment extends Fragment {
             }
         });
 
+        //handle share button
+        mShare = (FloatingActionButton)view.findViewById(R.id.fab_rating_share);
+        mShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                ImageView image = new ImageView(getContext());
+//                image.setImageBitmap(viewToBitmap(mRatingCard));
+//                new AlertDialog.Builder(getContext()).setView(image).setTitle("Test").show();
+                shareOnSocialMedia(viewToBitmap(mRatingCard));
+            }
+        });
+
         return view;
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        TransitionManager.beginDelayedTransition(mRatingGroup);
-        mRating.setVisibility(View.VISIBLE);
+    //converts rating card to bitmap
+    private static Bitmap viewToBitmap(View view){
+        //Define bitmap with same size as view
+        Bitmap output = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
+        //bind canvas to it
+        Canvas canvas = new Canvas(output);
+        //get view's background
+        Drawable bg = view.getBackground();
+        if(bg!=null) bg.draw(canvas);
+        else canvas.drawColor(Color.WHITE);
+        view.draw(canvas);
+        return output;
+    }
+
+    //shares the rated dog on social media
+    private void shareOnSocialMedia(Bitmap image){
+        //get image currently in the imageview
+//        Bitmap image = ((BitmapDrawable)mPhoto.getDrawable()).getBitmap();
+
+        //share image
+        String path = MediaStore.Images.Media.insertImage(getContext().getContentResolver(), image, "Dog", null);
+        Uri uri = Uri.parse(path);
+        Intent share = new Intent(Intent.ACTION_SEND);
+        share.setType("image/*");
+        share.putExtra(Intent.EXTRA_STREAM, uri);
+        share.putExtra(Intent.EXTRA_TEXT, "test text");
+        getContext().startActivity(Intent.createChooser(share, "Share your rating!"));
     }
 
     public interface RatingFragListener{
